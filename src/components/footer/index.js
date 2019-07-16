af08e538fda69dea2aa4a63ac7350b11bef5ef30
Mareli <ambra.lioness@gmail.com>
2019-07-16 14:40:46 +0200

    update layout of footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import './footer.css';
+import React, { Component } from "react"
+import "./footer.css"
 
 class Footer extends Component {
-    render() {
-        return (
-            <div className="footer">
-                <div className="container">
-                    <div className="footer__container">
-                        <div className="footer__col">Footer Column</div>
-                        <div className="footer__col">Footer Column</div>
-                        <div className="footer__col">Footer Column</div>
-                        <div className="footer__col">Footer Column</div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="footer">
+				<div className="container">
+					<div className="footer__container">
+						<div className="footer__col">
+							Contact me:
+							<a href="mailto:basson.mareli@gmail.com">basson.mareli@gmail.com</a>
+						</div>
+						<div className="footer__col">
+							<div className="icon">a</div>
+							<div className="icon">a</div>
+							<div className="icon">a</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default Footer;
+export default Footer
