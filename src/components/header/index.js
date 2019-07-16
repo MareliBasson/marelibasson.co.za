713fa81c98c73ce92e95c78bea94b015dd262d3e
Mareli <ambra.lioness@gmail.com>
2019-07-16 14:40:29 +0200

    update layout of header component and add fontawesome

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import Menu from 'components/menu';
-import './header.css';
+import React, { Component } from "react"
+import Menu from "components/menu"
+import "./header.css"
 
 class Header extends Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="container">
-                    <div className="header__container">
-                        <div className="header__logo">Page Logo</div>
-                        <Menu />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="header">
+				<div className="header__left">
+					<Menu />
+				</div>
+				<div className="header__right">
+					<div className="icon-links">
+						<i className="fab fa-codepen" />
+						<i className="fab fa-github" />
+						<i className="fab fa-linkedin" />
+					</div>
+					<div className="about">Mareli Basson</div>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default Header;
+export default Header
