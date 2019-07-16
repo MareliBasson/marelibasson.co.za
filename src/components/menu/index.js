bff1036de110817f06ba4776cd8e7e8234c485d0
Mareli <ambra.lioness@gmail.com>
2019-07-16 15:57:29 +0200

    update structure of menu by adding slide out drawer

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -4,14 +4,29 @@ import "./menu.css"
 import { Link, animateScroll as scroll } from "react-scroll"
 
 class Menu extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			menuOpen: false
+		}
+
+		this.handleToggle = this.handleToggle.bind(this)
+	}
+
+	handleToggle() {
+		this.setState({
+			menuOpen: !this.state.menuOpen
+		})
+	}
+
 	render() {
 		return (
 			<div className="menu">
-				<div className="menu-toggle">
+				<div className="menu-toggle" onClick={this.handleToggle}>
 					<div className="menu-icon" />
 				</div>
 
-				<div className="menu-container">
+				<div className={`menu-container ${this.state.menuOpen ? "open" : ""}`}>
 					<NavLink activeClassName="active" exact to="/">
 						Home
 					</NavLink>
@@ -19,8 +34,8 @@ class Menu extends Component {
 						Gallery
 					</NavLink>
 					{/* <NavLink activeClassName="active" to="/page2">
-					Page 2
-				</NavLink> */}
+                        Page 2
+                    </NavLink> */}
 					<Link activeClass="active" to="section2" spy={true} smooth={true} offset={-70} duration={500}>
 						test
 					</Link>
