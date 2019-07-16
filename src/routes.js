4887cbc048e7614c1fb0588ce840005e7b96be7a
Mareli <ambra.lioness@gmail.com>
2019-07-16 16:50:06 +0200

    remove removed page reference

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import HomePage from "pages/home"
 import PageOne from "pages/page1"
-import PageTwo from "pages/page2"
 
 const Routes = () => (
 	<Router>
