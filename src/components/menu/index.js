31e430eb024f0a91a6679bb28abc6b27d68f171f
Mareli <ambra.lioness@gmail.com>
2019-07-16 16:12:55 +0200

    clean up styling on global links and menu component

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react"
 import { NavLink } from "react-router-dom"
 import "./menu.css"
-import { Link, animateScroll as scroll } from "react-scroll"
+import { Link } from "react-scroll"
 
 class Menu extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			menuOpen: false
+			menuOpen: true
 		}
 
 		this.handleToggle = this.handleToggle.bind(this)
@@ -28,16 +28,79 @@ class Menu extends Component {
 
 				<div className={`menu-container ${this.state.menuOpen ? "open" : ""}`}>
 					<NavLink activeClassName="active" exact to="/">
-						Home
+						<i className="fa fa-home" /> Home
 					</NavLink>
 					<NavLink activeClassName="active" to="/page1">
-						Gallery
+						<i className="fa fa-images" /> Gallery
 					</NavLink>
 					{/* <NavLink activeClassName="active" to="/page2">
                         Page 2
                     </NavLink> */}
-					<Link activeClass="active" to="section2" spy={true} smooth={true} offset={-70} duration={500}>
-						test
+					<Link
+						activeClass="active"
+						to="section1"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 1
+					</Link>
+					<Link
+						activeClass="active"
+						to="section2"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 2
+					</Link>
+					<Link
+						activeClass="active"
+						to="section3"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 3
+					</Link>
+					<Link
+						activeClass="active"
+						to="section4"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 4
+					</Link>
+					<Link
+						activeClass="active"
+						to="section5"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 5
+					</Link>
+					<Link
+						activeClass="active"
+						to="section6"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onClick={this.handleToggle}
+					>
+						Section 6
 					</Link>
 				</div>
 			</div>
