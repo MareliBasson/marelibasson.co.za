f107073c4a3e0b1b41c8d996b6bbb7eddddbdce7
Mareli <ambra.lioness@gmail.com>
2019-07-17 13:33:14 +0200

    update content of landing section

--- src/sections/landing/index.js
+++ src/sections/landing/index.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react"
 import Section from "components/section"
 import background from "../../images/bg-landing.jpg"
+import profile from "../../images/profile.jpg"
 import "./landing.css"
 
 class Landing extends Component {
 	render() {
 		return (
 			<Section id="landing" background={background}>
-				<h1>Landing</h1>
+				<div className="intro">
+					<h1>Hi, I'm Mareli</h1>
+					<h2>A front-end developer & UX enthusiast from Cape Town </h2>
+					<div className="image" style={{ backgroundImage: `url(${profile})` }} />
+				</div>
 			</Section>
 		)
 	}
