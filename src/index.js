dfd56b98f009de07404328a31cd08b8c5adce9fb
Mareli <ambra.lioness@gmail.com>
2019-07-17 13:32:36 +0200

    install react-lightgallery

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import registerServiceWorker from "./registerServiceWorker"
 // Stylesheets
 import "normalize.css"
 import "./index.css"
+import "lightgallery.js/dist/css/lightgallery.css"
 
 ReactDOM.render(<Routes />, document.getElementById("root"))
 registerServiceWorker()
