e42f833824a8597959dede0ba4244bde42d4c672
Mareli <ambra.lioness@gmail.com>
2019-07-30 15:15:39 +0200

    create convertor component

---
+++ src/components/convertor/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react"
+
+import "./convertor.css"
+
+class Convertor extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			input: "",
+			copySuccess: ""
+		}
+	}
+
+	handleChange = e => {
+		this.setState({
+			input: e.target.value.replace(/\s+/g, "-").toLowerCase(),
+			copySuccess: ""
+		})
+	}
+
+	copyToClipboard = e => {
+		e.preventDefault()
+		this.filename.select()
+		document.execCommand("copy")
+		e.target.focus()
+		this.setState({ copySuccess: "Copied!" })
+	}
+
+	render() {
+		return (
+			<div className="container convertor">
+				<form>
+					<input
+						ref={filename => (this.filename = filename)}
+						name="name"
+						value={this.state.input}
+						onChange={e => this.handleChange(e)}
+					/>
+					<div class="copy-btn">
+						<button onClick={this.copyToClipboard} type="submit">
+							Copy
+						</button>
+						<div>{this.state.copySuccess}</div>
+					</div>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default Convertor
--- src/pages/page1/index.js
+++ src/pages/page1/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react"
 import PageTemplate from "components/page-template"
 import OtherSkills from "sections/other-skills"
+import Convertor from "components/convertor"
 import "./page1.css"
 
 class PageOne extends Component {
 	render() {
 		return (
 			<PageTemplate pageHead="Gallery">
-				<OtherSkills />
+				<Convertor />
+				{/* <OtherSkills /> */}
 			</PageTemplate>
 		)
 	}
