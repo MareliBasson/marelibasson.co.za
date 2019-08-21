95c078e9015d2f7aba22185653eb577dedafa8c4
Mareli <ambra.lioness@gmail.com>
2019-08-21 16:27:35 +0200

    update layout of footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,16 +6,20 @@ class Footer extends Component {
 		return (
 			<div className="footer" id="footer">
 				<div className="container">
-					<div className="footer__container">
-						<div className="footer__col">
-							Contact me:
-							<a href="mailto:basson.mareli@gmail.com">basson.mareli@gmail.com</a>
-						</div>
-						<div className="footer__col">
-							Disclaimer, this template design is based on one I bought through Creative Market. I have however completely
-							rebuilt it using ReactJS and netlify for deployment. You can view the repo for this project here:
-							<a href="">marelibasson.com Repo</a>
-						</div>
+					<div className="contact">
+						<a href="mailto:basson.mareli@gmail.com">basson.mareli@gmail.com</a>
+					</div>
+					<div className="disclaimer">
+						<h5>Disclaimer</h5> <br />
+						This template design is based on one I bought from Creative Market. I have however completely rebuilt it using
+						ReactJS and is hosting it using{" "}
+						<a href="https://www.netlify.com/" target="_blank">
+							Netlify
+						</a>
+						. You can view the repo for this project here:
+						<a href="https://github.com/MareliBasson/marelibasson.com" target="_blank" rel="noopener noreferrer">
+							marelibasson.com Repo
+						</a>
 					</div>
 				</div>
 			</div>
