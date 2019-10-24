320d5ea32fa3a8934f467f5fafcd82e234fe73c9
Mareli <ambra.lioness@gmail.com>
2019-10-24 14:52:26 +0200

    clean up functionality and add file extension field

--- src/components/filename-convertor/index.js
+++ src/components/filename-convertor/index.js
@@ -1,68 +1,69 @@
-import React, { Component } from 'react';
-import './filename-convertor.css';
+import React, { Component } from "react"
+import "./filename-convertor.css"
 
-var _ = require('lodash');
+var _ = require("lodash")
 
 class FilenameConvertor extends Component {
 	constructor(props) {
-		super(props);
+		super(props)
 		this.state = {
-			filenameInput: '',
-			copySuccess: 'Copy',
-			spaces: 'dash',
-			prefix: '',
-			suffix: '',
-			formattedName: ''
-		};
+			filenameInput: "",
+			copySuccess: "Copy",
+			spaces: "dash",
+			prefix: "",
+			suffix: "",
+			extion: "",
+			formattedName: ""
+		}
 
-		this.formatName = this.formatName.bind(this);
-		this.handleChange = this.handleChange.bind(this);
+		this.formatName = this.formatName.bind(this)
+		this.handleChange = this.handleChange.bind(this)
 	}
 
 	handleChange = e => {
 		const name = e.target.name,
-			val = e.target.value.trim();
+			val = e.target.value
 
 		this.setState({
 			[name]: val
-		});
-	};
+		})
+	}
 
 	formatName = e => {
-		const { filenameInput, spaces, prefix, suffix } = this.state;
+		const { filenameInput, spaces, prefix, suffix, extension } = this.state
 
-		const spaceSymbol = spaces === 'underscore' ? '_' : spaces === 'dash' ? '-' : '';
+		const spaceSymbol = spaces === "underscore" ? "_" : spaces === "dash" ? "-" : ""
 
-		const fullname = `${prefix} ${filenameInput} ${suffix}`;
+		const fullname = `${prefix ? prefix + " " : ""}${filenameInput}${suffix ? " " + suffix : ""}${extension ? "." + extension : ""}`
 
 		const formatFullname = fullname
 			.trim()
 			.toLowerCase()
-			.replace(/[-_]/g, ' ')
-			.replace(/[.,`"'“”/#!$%^&*;:{}=~()]/g, '');
+			.replace(/[-_]/g, " ")
+			.replace(/[,`"'“”/#!$%^&*;:{}=~()]/g, "")
 
 		this.setState({
 			formattedName: formatFullname.replace(/\s+/g, spaceSymbol)
-		});
-	};
+		})
+	}
 
 	componentDidUpdate(prevProps, prevState) {
 		if (!_.isEqual(prevState, this.state)) {
-			this.formatName();
+			this.formatName()
 		}
 	}
 
 	copyToClipboard = e => {
-		e.preventDefault();
+		e.preventDefault()
 
-		this.filename.select();
-		document.execCommand('copy');
-		e.target.focus();
-		this.setState({ copySuccess: 'Copied!' });
-	};
+		this.filename.select()
+		document.execCommand("copy")
+		e.target.focus()
+		this.setState({ copySuccess: "Copied!" })
+	}
 
 	render() {
-		const { filenameInput, formattedName, copySuccess, spaces, prefix, suffix } = this.state;
+		const { filenameInput, formattedName, copySuccess, spaces, prefix, suffix, extension } = this.state
 
 		return (
 			<div className="container convertor">
@@ -77,7 +78,7 @@ class FilenameConvertor extends Component {
 								name="filenameInput"
 								value={filenameInput}
 								onClick={e => {
-									this.setState({ copySuccess: 'Copy' });
+									this.setState({ copySuccess: "Copy" })
 								}}
 								onChange={this.handleChange}
 							/>
@@ -85,8 +86,8 @@ class FilenameConvertor extends Component {
 							<button
 								type="submit"
 								onClick={e => {
-									e.preventDefault();
-									this.setState({ filenameInput: '', copySuccess: 'Copy' });
+									e.preventDefault()
+									this.setState({ filenameInput: "", copySuccess: "Copy" })
 								}}
 							>
 								<i className="fas fa-backspace" />
@@ -96,8 +97,8 @@ class FilenameConvertor extends Component {
 						<div
 							className="copy-btn"
 							onClick={e => {
-								if (formattedName !== '') {
-									this.copyToClipboard(e);
+								if (formattedName !== "") {
+									this.copyToClipboard(e)
 								}
 							}}
 						>
@@ -108,42 +109,89 @@ class FilenameConvertor extends Component {
 
 					<div className="settings">
 						<div className="sections spaces">
-							<strong>Replace spaces with:</strong>
-							<br />
-							<input
-								type="radio"
-								name="spaces"
-								id="dash"
-								value="dash"
-								checked={spaces === 'dash'}
-								onChange={this.handleChange}
-							/>
-							<label htmlFor="dash">Dashes</label>
-							<input
-								type="radio"
-								name="spaces"
-								id="underscore"
-								value="underscore"
-								onChange={this.handleChange}
-							/>
-							<label htmlFor="underscore">Underscores</label>
-							<input type="radio" name="spaces" id="remove" value="remove" onChange={this.handleChange} />
-							<label htmlFor="remove">Remove them</label>
+							<h6>Replace spaces with:</h6>
+							<div>
+								<input
+									type="radio"
+									name="spaces"
+									id="dash"
+									value="dash"
+									checked={spaces === "dash"}
+									onChange={this.handleChange}
+								/>
+								<label htmlFor="dash">Dashes</label>
+							</div>
+							<div>
+								<input type="radio" name="spaces" id="underscore" value="underscore" onChange={this.handleChange} />
+								<label htmlFor="underscore">Underscores</label>
+							</div>
+							<div>
+								<input type="radio" name="spaces" id="remove" value="remove" onChange={this.handleChange} />
+								<label htmlFor="remove">Remove them</label>
+							</div>
 						</div>
 
 						<div className="fixes">
-							<label htmlFor="prefix">Prefix: </label>
-							<input type="text" name="prefix" value={prefix} onChange={this.handleChange} />
-						</div>
-						<div className="fixes">
-							<label htmlFor="suffix">Suffix: </label>
-							<input type="text" name="suffix" value={suffix} onChange={this.handleChange} />
+							<div>
+								<label htmlFor="prefix">Prefix: </label>
+								<br />
+								<input type="text" name="prefix" value={prefix} onChange={this.handleChange} />
+
+								<button
+									type="submit"
+									onClick={e => {
+										e.preventDefault()
+										this.setState({ prefix: "" })
+									}}
+								>
+									<i className="fas fa-backspace" />
+								</button>
+							</div>
+							<div>
+								<label htmlFor="suffix">Suffix: </label>
+								<br />
+								<input type="text" name="suffix" value={suffix} onChange={this.handleChange} />
+								<button
+									type="submit"
+									onClick={e => {
+										e.preventDefault()
+										this.setState({ suffix: "" })
+									}}
+								>
+									<i className="fas fa-backspace" />
+								</button>
+							</div>
+
+							<div>
+								<label htmlFor="extension">File extension: </label>
+								<br />
+								<input type="text" name="extension" value={extension} onChange={this.handleChange} />
+								<button
+									type="submit"
+									onClick={e => {
+										e.preventDefault()
+										this.setState({ extension: "" })
+									}}
+								>
+									<i className="fas fa-backspace" />
+								</button>
+							</div>
+							<button
+								type="submit"
+								onClick={e => {
+									e.preventDefault()
+									this.setState({ prefix: "", suffix: "", extension: "" })
+								}}
+							>
+								<i class="far fa-times-circle" />
+								Clear All
+							</button>
 						</div>
 					</div>
 				</form>
 			</div>
-		);
+		)
 	}
 }
 
-export default FilenameConvertor;
+export default FilenameConvertor
